9ed4b64568a8b5c08e9ca9a1aef66ab92629d982
KGY <44287647+kwongiyeon@users.noreply.github.com>
2024-05-23 18:46:10 +0900

    Update Member.java

--- CLI-CafeOrder/src/main/java/OrderSystem/Member.java
+++ CLI-CafeOrder/src/main/java/OrderSystem/Member.java
@@ -3,10 +3,12 @@ package OrderSystem;
 public class Member extends Customer {
     private String nickName;
     private int number;
+    private int couponCount;
 
     public Member(String nickName, int number) {
         this.nickName = nickName;
         this.number = number;
+        this.couponCount = 0;
     }
 
     public String getNickName() {
@@ -23,5 +25,21 @@ public class Member extends Customer {
 
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    public int getCouponCount() {
+        return couponCount;
+    }
+
+    public void addCoupon(int count) {
+        this.couponCount += count;
+    }
+
+    public void useCoupon(int count) {
+        if (this.couponCount >= count) {
+            this.couponCount -= count;
+        } else {
+            System.out.println("쿠폰이 부족합니다.");
+        }
     }
 }
