ba4bb3f82cf9b8e1f4eaaa541296646c3f01741c
KGY <44287647+kwongiyeon@users.noreply.github.com>
2024-05-23 18:45:23 +0900

    Update and rename Cafe.java to CafeMenu.java

--- CLI-CafeOrder/src/main/java/OrderSystem/CafeMenu.java
+++ CLI-CafeOrder/src/main/java/OrderSystem/CafeMenu.java
@@ -1,24 +1,14 @@
 package OrderSystem;
 
-//부모 클래스=카페
-public class Cafe {
+//부모 클래스=카페메뉴
+public class CafeMenu {
     protected String name;
     protected double price;
 
-    public Cafe(String name, double price) {
+    public CafeMenu(String name, double price) {
         this.name = name;
         this.price = price;
     }
-
-//    // 이름 설정자
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    // 이름 불러오기
-//    public String getName() {
-//        return name;
-//    }
 
     // 가격 설정자(변경 가능)
     public void setPrice(double price) {
