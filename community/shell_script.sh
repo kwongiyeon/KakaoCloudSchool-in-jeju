7208ebcd87129d7373d917f0f6c75baf741d3312
KGY <44287647+kwongiyeon@users.noreply.github.com>
2024-06-25 11:08:31 +0900

    Update shell_script.sh

--- community/shell_script.sh
+++ community/shell_script.sh
@@ -7,3 +7,11 @@ npm install cors               // 서로 다른 도메인, 프로토콜, 포트 
 npm install cookie-parser      // 쿠키 구현 -> 쿠키 생성, 삭제 등 요청 받은 구문 분석하고 추출, request(req) 객체에 cookies 속성이 부여
 npm install express-session    // 세션 구현 -> 세션 데이터는 쿠키 자체에 저장되지 않고 세션 ID만 저장, 즉 서버 측에 저장됨, 서버에서 sid로 사용자 식별
 npm install mysql2             // 데이터베이스 설치
+
+// package.json에 기입
+"scripts": {
+    "start": "node app.js"
+  }
+// 프론트엔드, 백엔드 전용 app.js에 각각 기입
+
+npm start  //서버 시작
