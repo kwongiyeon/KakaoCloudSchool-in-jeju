bd694176ca20e71ca07eae94f376fd19e7282cf1
KGY <44287647+kwongiyeon@users.noreply.github.com>
2024-07-11 15:46:01 +0900

    Delete Database(MySQL) directory

--- Database(MySQL)/authenticate.js
+++
@@ -1,8 +0,0 @@
-const authenticate = (req, res, next) => {
-    if (!req.session.user) {
-      return res.status(401).json({ message: '로그인 해주세요.' });
-    }
-    next(); // 로그인되어 있으면 다음 미들웨어나 라우트 핸들러로 이동
-  };
-  
-  export default authenticate;
